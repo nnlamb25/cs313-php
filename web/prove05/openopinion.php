528948c4430e134ae8c128411b65997ac1e3cc4e
nnlamb25 <nnlamb25@gmail.com>
2018-02-08 16:49:19 -0700

    Added error catching

--- web/prove05/openopinion.php
+++ web/prove05/openopinion.php
@@ -5,7 +5,16 @@ $user = 'bruigbiqkmqflz';
 $password = 'f16ade2ac40bc9bc38241e497207d16178f08200e4d2e4820c893760733dd068';
 $host = 'ec2-54-235-240-126.compute-1.amazonaws.com';
 $dbname = 'd9odltre339tgq';
-$myDatabase = new PDO("pgsql:host=".$host."; dbname=".$dbname, $user, $pass);
+
+try {
+    $myDatabase = new PDO("pgsql:host=".$host."; dbname=".$dbname, $user, $pass);
+    $myDatabase->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "connected successfully";
+}
+catch(PDOException $e)
+{
+    echo "Connection Failed: " . $e->getMessage();
+}
 
 //$myDatabase = new PDO('pgsql:host=ec2-54-235-240-126.compute-1.amazonaws.com;dbname=d9odltre339tgq', $user, $password);
 //$dbconn = pg_connect("host=ec2-54-235-240-126.compute-1.amazonaws.com port=5432 dbname=d9odltre339tgq user=$user password=$password")
