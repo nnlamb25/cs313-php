ddf51e3e7c6fc3a1f1f4cba0b6a6e18535ccddc5
nnlamb25 <nnlamb25@gmail.com>
2018-02-08 16:45:32 -0700

    searching for the bug..

--- web/prove05/openopinion.php
+++ web/prove05/openopinion.php
@@ -5,7 +5,7 @@ $user = 'bruigbiqkmqflz';
 $password = 'f16ade2ac40bc9bc38241e497207d16178f08200e4d2e4820c893760733dd068';
 $host = 'ec2-54-235-240-126.compute-1.amazonaws.com';
 $dbname = 'd9odltre339tgq';
-//$myDatabase = new PDO("pgsql:host=".$host."; dbname=".$dbname, $user, $pass);
+$myDatabase = new PDO("pgsql:host=".$host."; dbname=".$dbname, $user, $pass);
 
 //$myDatabase = new PDO('pgsql:host=ec2-54-235-240-126.compute-1.amazonaws.com;dbname=d9odltre339tgq', $user, $password);
 //$dbconn = pg_connect("host=ec2-54-235-240-126.compute-1.amazonaws.com port=5432 dbname=d9odltre339tgq user=$user password=$password")
