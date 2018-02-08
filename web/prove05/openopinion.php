768c8fb84c005d16a40e8e6136225b3d14a2e7a8
nnlamb25 <nnlamb25@gmail.com>
2018-02-08 16:40:15 -0700

    trying hard to connect the DB

--- web/prove05/openopinion.php
+++ web/prove05/openopinion.php
@@ -3,7 +3,12 @@ session_start();
 
 $user = 'bruigbiqkmqflz';
 $password = 'f16ade2ac40bc9bc38241e497207d16178f08200e4d2e4820c893760733dd068';
-$myDatabase = new PDO('pgsql:host=ec2-54-235-240-126.compute-1.amazonaws.com;dbname=d9odltre339tgq', $user, $password);
+$host = 'ec2-54-235-240-126.compute-1.amazonaws.com';
+$dbname = 'd9odltre339tgq';
+$myDatabase = new PDO("pgsql:host=".$host."; dbname=".$dbname, $user, $pass);
+
+//$myDatabase = new PDO('pgsql:host=ec2-54-235-240-126.compute-1.amazonaws.com;dbname=d9odltre339tgq', $user, $password);
+//$dbconn = pg_connect("host=ec2-54-235-240-126.compute-1.amazonaws.com port=5432 dbname=d9odltre339tgq user=$user password=$password")
 
 } catch (PDOException $e) {
   echo "Error!: " . $e->getMessage();
