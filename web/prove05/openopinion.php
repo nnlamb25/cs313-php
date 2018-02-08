80eb483b651beeb156909eafcf0b98063aadfd2b
nnlamb25 <nnlamb25@gmail.com>
2018-02-08 16:27:13 -0700

    Hooked up heroku database

--- web/prove05/openopinion.php
+++ web/prove05/openopinion.php
@@ -1,5 +1,16 @@
 <?php
 session_start();
+
+$user = 'bruigbiqkmqflz';
+  $password = 'f16ade2ac40bc9bc38241e497207d16178f08200e4d2e4820c893760733dd068';
+  $myDatabase = new PDO('pgsql:host=ec2-54-235-240-126.compute-1.amazonaws.com
+;dbname=d9odltre339tgq', $user, $password);
+
+} catch (PDOException $e) {
+  echo "Error!: " . $e->getMessage();
+  die();
+}
+
 ?>
 <!DOCTYPE html>
 <html>
@@ -9,8 +20,16 @@ session_start();
     </head>
     <body>
         <div id="header">
-            <h1>Hello Open Opinion!</h1>
+            <h1>Open Opinion</h1>
         </div>
+        
+        <?php
+        
+        foreach ($myDatabase->query('SELECT * FROM public.user;') as $row)
+        {
+            echo $row['username'];
+        }
+
         
     </body>
 </html>
