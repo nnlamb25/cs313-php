8dbb6e1ab1430855ada3aa4e49c718c69f1fbe9a
nnlamb25 <nnlamb25@gmail.com>
2018-02-08 19:06:03 -0700

    debugging..........

--- web/prove05/opinionpost.php
+++ web/prove05/opinionpost.php
@@ -41,7 +41,7 @@ $postID = $_GET['id'];
             }
             
             echo '<div id="comments">';
-            foreach ($myDatabase->query("SELECT * FROM public.post_comment WHERE post_id='2' AND reply_to_comment_id='7';") as $comment)//"SELECT * FROM public.post_comment WHERE post_id='" . $post['id'] . "' AND reply_to_comment_id='7';") as $comment))
+            foreach ($myDatabase->query("SELECT * FROM public.post_comment WHERE post_id='" . $post['id'] ."' AND reply_to_comment_id IS NULL;") as $comment)//"SELECT * FROM public.post_comment WHERE post_id='" . $post['id'] . "' AND reply_to_comment_id='7';") as $comment))
             {
                 echo '<div class="comment">' . $comment['comment_text'];
                 //foreach ($myDatabase->query("SELECT * FROM public.post_comment WHERE post_id='" . $post['id'] . "' AND reply_to_comment='" . $comment['id'] . "';") as $reply)
