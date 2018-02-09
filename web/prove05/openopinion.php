371dee4da73971f51d048c0df294f87db7931e8a
nnlamb25 <nnlamb25@gmail.com>
2018-02-08 17:12:57 -0700

    dumb bug

--- web/prove05/openopinion.php
+++ web/prove05/openopinion.php
@@ -32,7 +32,7 @@ catch(PDOException $e)
         foreach ($myDatabase->query('SELECT * FROM public.opinion_post;') as $row)
         {
             echo $row['id'];
-            echo '<a href="opinionpost.php?id="' . $row['id']. '">' . $row['post_title'] . '<br>';
+            echo '<a href="opinionpost.php?id=' . $row['id']. '">' . $row['post_title'] . '<br>';
             //echo $row['post_title'] . '<br>';
             
         }
