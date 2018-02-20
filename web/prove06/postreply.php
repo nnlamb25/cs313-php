de831fb89b0b162ae257d1ced24160ea4b43cd82
nnlamb25 <nnlamb25@gmail.com>
2018-02-20 16:18:38 -0700

    Fixed spelling error on filename

--- web/prove06/postreply.php
+++ web/prove06/postreply.php
@@ -4,7 +4,7 @@ session_start();
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-require("dpConnect.php");
+require("dbConnect.php");
 $myDatabase = get_db();
 
 $postID = $_SESSION['postid'];
