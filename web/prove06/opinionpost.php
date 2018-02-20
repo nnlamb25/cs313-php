50a024356f116bb9d13bc74f68d0ed795aaf0a94
nnlamb25 <nnlamb25@gmail.com>
2018-02-20 16:08:57 -0700

    Now logs into database from another file to add security

--- web/prove06/opinionpost.php
+++ web/prove06/opinionpost.php
@@ -1,19 +1,8 @@
 <?php
 session_start();
 
-$user = 'bruigbiqkmqflz';
-$password = 'f16ade2ac40bc9bc38241e497207d16178f08200e4d2e4820c893760733dd068';
-$host = 'ec2-54-235-240-126.compute-1.amazonaws.com';
-$dbname = 'd9odltre339tgq';
-
-try {
-    $myDatabase = new PDO("pgsql:host=".$host."; dbname=".$dbname, $user, $password);
-    $myDatabase->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}
-catch(PDOException $e)
-{
-    echo "Connection Failed: " . $e->getMessage();
-}
+require("dpConnect.php");
+$myDatabase = get_db();
 
 $postID = $_GET['id'];
 
