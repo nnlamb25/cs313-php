c99b45d4adc5c8bc7cffd3a9c1aad315133fa772
nnlamb25 <nnlamb25@gmail.com>
2018-02-20 17:40:32 -0700

    Users can now delete their own comments and mods can delete any comment

--- web/prove06/deletecomment.php
+++ web/prove06/deletecomment.php
@@ -17,29 +17,24 @@ $postID = $_GET['postID'];
     <?php
     if(isset($_SESSION['username']))
     {
-        echo 'SET<br>';
         foreach($myDatabase->query('SELECT * FROM public.post_comment;') as $row)
         {
             if($row['id'] == $commentID)
             {
-                echo 'HERE<br>';
                 foreach($myDatabase->query('SELECT * FROM public.user') as $user)
                 {
                     if($_SESSION['isMod'] || ($user['username'] == $_SESSION['username'] && $row['poster_id'] == $user['id']))
                     {
-                        echo 'INSIDE<br>';
                         $delete = "DELETE FROM public.post_comment WHERE id = :id";
                         $stmt = $myDatabase->prepare($delete);
                         $stmt->bindValue(':id', $commentID, PDO::PARAM_INT);
                         $stmt->execute();
-                        echo 'DONE<br>';
                     }
                 }
             }
         }
     }
-    echo 'END<br>';
     
-    //echo "<script>window.location = 'opinionpost.php?id=$postID' </script>";
+    echo "<script>window.location = 'opinionpost.php?id=$postID' </script>";
     ?>
 </html>
