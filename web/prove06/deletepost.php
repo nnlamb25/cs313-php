56fb563e5226a0e182cf021740655bad73d8cdc6
nnlamb25 <nnlamb25@gmail.com>
2018-02-20 17:06:10 -0700

    Only allows mods to original posters to delete posts

--- web/prove06/deletepost.php
+++ web/prove06/deletepost.php
@@ -14,10 +14,26 @@ $postID = $_GET['id'];
 <!DOCTYPE html>
 <html>
     <?php
-    $deleteStmt = "DELETE FROM public.opinion_post WHERE id = '$postID'";
-    pg_query($deleteStmt);
+    if(isset($_SESSION['username']))
+    {
+        foreach($myDatabase->query('SELECT * FROM public.opinion_post;') as $row)
+        {
+            if($row['id'] == $postID)
+            {
+                foreach($myDatabase->query('SELECT * FROM public.user') as $user)
+                {
+                    if(($user['username'] == $_SESSION['username'] && $row['poster_id'] == $user['id']) || $_SESSION['isMod'])
+                    {
+                        $delete = "DELETE FROM public.opinion_post WHERE id = :id";
+                        $stmt = $myDatabase->prepare($delete);
+                        $stmt->bindValue(':id', $postID, PDO::PARAM_INT);
+                        $stmt->execute();
+                    }
+                }
+            }
+        }
+    }
     
     echo "<script>window.location = 'openopinion.php' </script>";
-    
     ?>
 </html>
